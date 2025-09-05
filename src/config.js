ff57a0b11a167d0a4cc6e3bf55f7016de2e38417
unknown <ngudeta87@gmail.com>
2025-09-05 22:11:35 +0300

    project

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const PORT = process.env.PORT || 3000;
-export const MONGO_URI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/projectDB';
+export const MONGO_URI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/user';
 export const JWT_SECRET = process.env.JWT_SECRET || 'your_secret_key';
